eddaf4945b0d525024cd2447feb51e2a4d1e9587
ian.vinicius <ian.vinicius@tascominformatica.com.br>
2022-01-12 20:08:22 -0300

    Create components of buttons

---
+++ src/components/Buttons/ArrowButton.svelte
@@ -0,0 +1,44 @@
+<script>
+  import { LeftArrow, RightArrow } from "../";
+
+  export let disabled = false;
+  export let btnType = 'next';
+  export let onClick = () => {};
+</script>
+
+<button class='arrow-button' disabled={disabled} on:click={() => onClick()}>
+  {#if btnType === 'back'}
+    <LeftArrow />
+  {:else}
+    <RightArrow />
+  {/if}
+</button>
+
+<style>
+  .arrow-button {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 4rem;
+    height: 3rem;
+    background: var(--colorTwo);
+    border-radius: .5rem;
+    border: .1rem solid var(--colorOne);
+    cursor: pointer;
+    transition: .32s ease-in-out;
+  }
+
+  .arrow-button:hover {
+    background: #fff;
+    box-shadow: 0 0 .5rem .4rem var(--colorThree);
+  }
+
+  .arrow-button:hover > svg > * {
+    stroke: var(--colorOne);
+  }
+
+  .arrow-button > svg {
+    width: 50%;
+    height: 50%;
+  }
+</style>
---
+++ src/components/Buttons/BackButton.svelte
@@ -0,0 +1,49 @@
+<script>
+  import { link } from "svelte-routing";
+  import { LeftArrow } from "../";
+
+  export let style = '';
+  export let href = '';
+</script>
+
+<a {href} use:link class="back-button" style={style}>
+  <LeftArrow />
+  <span>Voltar</span>
+</a>
+
+<style>
+  .back-button {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 6rem;
+    height: 3rem;
+    background: var(--colorTwo);
+    border-radius: .5rem;
+    border: .1rem solid var(--colorOne);
+    cursor: pointer;
+    transition: .32s ease-in-out;
+    color: #fff ;
+    padding: .0rem .8rem;
+    text-decoration: none;
+  }
+
+  .back-button:hover {
+    background: #fff;
+    box-shadow: 0 0 .5rem .4rem var(--colorThree);
+    color: var(--colorOne);
+  }
+
+  .back-button:hover > svg > * {
+    stroke: var(--colorOne);
+  }
+
+  .back-button > svg {
+    width: 30%;
+    height: 30%;
+  }
+
+  .back-button > span {
+    font-size: 1rem;
+  }
+</style>
