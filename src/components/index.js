759c81815f4239262a884b2f2ef0ed316d2ba33a
i4n-v <ianvinicioss@gmail.com>
2022-01-13 19:57:22 -0300

    Adding initial implementation of game page

--- src/components/index.js
+++ src/components/index.js
@@ -1,14 +1,16 @@
-import Logo from  './Logo/Logo.svelte';
-import ButtonMenu from './Buttons/ButtonMenu.svelte';
-import ArrowButton from './Buttons/ArrowButton.svelte';
-import BackButton from './Buttons/BackButton.svelte';
-import InfoContainer from './InfoContainer/InfoContainer.svelte';
-import AboutCard from './Cards/AboutCard.svelte';
-import LeftArrow from './Icons/LeftArrow.svelte';
-import RightArrow from './Icons/RightArrow.svelte';
-import Instagram from './Icons/Instagram.svelte';
-import Github from './Icons/Github.svelte';
-import Linkedin from './Icons/Linkedin.svelte';
+import Logo from "./Logo/Logo.svelte";
+import ButtonMenu from "./Buttons/ButtonMenu.svelte";
+import ArrowButton from "./Buttons/ArrowButton.svelte";
+import BackButton from "./Buttons/BackButton.svelte";
+import InfoContainer from "./InfoContainer/InfoContainer.svelte";
+import AboutCard from "./Cards/AboutCard.svelte";
+import LeftArrow from "./Icons/LeftArrow.svelte";
+import RightArrow from "./Icons/RightArrow.svelte";
+import Instagram from "./Icons/Instagram.svelte";
+import Github from "./Icons/Github.svelte";
+import Linkedin from "./Icons/Linkedin.svelte";
+import SelectedLetters from "./Cards/SelectedLetters.svelte";
+import PlayersCard from "./Cards/PlayersCard.svelte";
 
 export {
   Logo,
@@ -22,4 +24,6 @@ export {
   Instagram,
   Github,
   Linkedin,
-}
+  SelectedLetters,
+  PlayersCard,
+};
--- src/pages/Forca/Forca.svelte
+++ src/pages/Forca/Forca.svelte
@@ -1,14 +1,21 @@
 <script>
-
+  import { SelectedLetters, BackButton, PlayersCard } from "../../components";
 </script>
 
 <svelte:head>
   <title>Forca | Jogando</title>
 </svelte:head>
-<main>
-  Jogando Forca
+<main class="wrapper">
+  <div class="info-container shadow-container">
+    <SelectedLetters title="Letras escolhidas" letters={['a', 'b', 'c']} />
+    <BackButton href="/" style="position: absolute; top: 2rem; right: .7rem;" />
+    <PlayersCard players={[{name: 'player 1', points: 2, color: '#F04444'}, {name: 'player 2', points: 3, color: '#4489F0'}]} />
+  </div>
 </main>
 
 <style>
-	
+	.info-container {
+    min-height: 34.7rem;
+    position: relative;
+  }
 </style>
