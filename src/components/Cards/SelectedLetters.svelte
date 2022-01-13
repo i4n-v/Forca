521fef3bae057a779d393c8616572a132e8d5747
i4n-v <ianvinicioss@gmail.com>
2022-01-13 20:00:11 -0300

    Creating SelectedLetters component

---
+++ src/components/Cards/SelectedLetters.svelte
@@ -0,0 +1,54 @@
+<script>
+  export let title = "";
+  export let letters = [];
+</script>
+
+<div id="selected-letters">
+  <h2 class="title-ifn">{title}</h2>
+  <div>
+    {#each letters as letter}
+      <span>{letter}</span>
+    {/each}
+  </div>
+</div>
+
+<style>
+  #selected-letters {
+    position: absolute;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    left: 2rem;
+  }
+
+  #selected-letters > div {
+    width: 13rem;
+    height: 9rem;
+    padding: .5rem;
+    background: #fff;
+    border-radius: 0.5rem;
+    border: 0.2rem solid var(--colorOne);
+    display: flex;
+    flex-wrap: wrap;
+    gap: .5rem;
+  }
+
+  #selected-letters > div > span {
+    font-size: 1.2rem;
+    font-weight: bold;
+    color: var(--colorFive);
+  }
+
+  .title-ifn {
+    font-size: 1.3rem;
+  }
+
+  .title-ifn:after {
+    width: 1rem;
+    height: 1rem;
+    position: absolute;
+    bottom: -0.2rem;
+    left: -0.3rem;
+    border-radius: 0.2rem;
+  }
+</style>
