25e5ca6836d4d79cddfb1f6c73cb2385ad736d29
i4n-v <ianvinicioss@gmail.com>
2022-01-14 20:05:01 -0300

    Creating keyboard of letters to application

---
+++ src/components/Buttons/GameButton.svelte
@@ -0,0 +1,28 @@
+<script>
+  export let letter = '';
+</script>
+
+<button type="button" class="game-button">
+  {letter}
+</button>
+
+<style>
+  .game-button {
+    padding: .6rem;
+    width: 4rem;
+    text-align: center;
+    color: var(--colorFive);
+    background: #fff;
+    font-size: 1.3rem;
+    border: .1rem solid var(--colorOne);
+    border-radius: .5rem;
+    transition: .32s ease-in-out;
+    cursor: pointer;
+  }
+
+  .game-button:hover {
+    background: var(--colorTwo);
+    box-shadow: 0 0 .5rem .4rem var(--colorThree);
+    color: var(--colorOne);
+  }
+</style>
---
+++ src/components/KeyboardGame/KeyBoardGame.svelte
@@ -0,0 +1,42 @@
+<script>
+  import { GameButton } from '../'
+</script>
+
+<div class="keyboard">
+  <GameButton letter="A" />
+  <GameButton letter="B" />
+  <GameButton letter="C" />
+  <GameButton letter="D" />
+  <GameButton letter="E" />
+  <GameButton letter="F" />
+  <GameButton letter="G" />
+  <GameButton letter="H" />
+  <GameButton letter="I" />
+  <GameButton letter="J" />
+  <GameButton letter="K" />
+  <GameButton letter="L" />
+  <GameButton letter="M" />
+  <GameButton letter="N" />
+  <GameButton letter="O" />
+  <GameButton letter="P" />
+  <GameButton letter="Q" />
+  <GameButton letter="R" />
+  <GameButton letter="S" />
+  <GameButton letter="T" />
+  <GameButton letter="U" />
+  <GameButton letter="V" />
+  <GameButton letter="W" />
+  <GameButton letter="X" />
+  <GameButton letter="Y" />
+  <GameButton letter="Z" />
+</div>
+
+<style>
+  .keyboard {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    gap: .8rem;
+  }
+</style>
