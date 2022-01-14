156e6ccacdd75af66598e17fa2ca36a2f8ebf441
i4n-v <ianvinicioss@gmail.com>
2022-01-14 20:00:28 -0300

    Change directory of svgs components

---
+++ src/components/Buttons/ButtonModal.svelte
@@ -0,0 +1,32 @@
+<script>
+  import { link } from "svelte-routing";
+
+  export let href = "";
+</script>
+
+<a {href} use:link class="btn-modal">
+  <slot />
+</a>
+
+<style>
+  .btn-modal {
+    padding: 0.8rem 0.4rem;
+    width: 100vw;
+    max-width: 13rem;
+    border-radius: 0.6rem;
+    border: 0.1rem solid var(--colorOne);
+    background-color: var(--colorTwo);
+    color: #ffff;
+    font: bold 1rem var(--nunito);
+    text-decoration: none;
+    text-align: center;
+    overflow: hidden;
+    transition: 0.3s ease-in-out;
+  }
+
+  .btn-modal:hover {
+    background: #fff;
+    box-shadow: 0 0 0.5rem 0.4rem var(--colorThree);
+    color: var(--colorOne);
+  }
+</style>
